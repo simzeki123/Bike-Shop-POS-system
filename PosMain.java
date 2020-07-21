4493c7778cdb3435d4e533d2c60c360c4ccd7f01
enderlord1583 <40665160+enderlord1583@users.noreply.github.com>
2020-07-21 17:18:00 +0800

    First commit

---
+++ PosMain.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)PosMain.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/7/20
+ */
+
+public class PosMain {
+        
+    /**
+     * Creates a new instance of <code>PosMain</code>.
+     */
+    public PosMain() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ monthly_report.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)monthly_report.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/7/20
+ */
+
+
+public class monthly_report {
+
+    public monthly_report() {
+    }
+    
+    
+}
