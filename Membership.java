aa4e684833ad784c5d6e45ead25e9c6961776b1a
StitchBae <68591641+StitchBae@users.noreply.github.com>
2020-07-22 22:19:07 +0800

    Add files via upload

---
+++ Membership.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Membership.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/7/22
+ */
+
+
+public class Membership {
+
+    public Membership() {
+    }
+    
+    
+}
---
+++ Payment.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Payment.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/7/22
+ */
+
+
+public class Payment {
+
+    public Payment() {
+    }
+    
+    
+}
