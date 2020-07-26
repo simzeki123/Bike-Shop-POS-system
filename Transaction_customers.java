b22c96376732506b9034f237366dd476e9420724
enderlord1583 <simzeki1583@gmail.com>
2020-07-27 00:20:03 +0800

    fixed class names

--- Transaction_customers.java
+++ Transaction_customers.java
@@ -7,65 +7,10 @@
  */
 
 
-public class Transaction_Customers {
+public class Transaction_customers {
 
-    public Transaction_Customers() {
+    public Transaction_customers() {
     }
     
     
 }
- No newline at end of file
-/**
- * @(#)Transaction_staff.java
- *
- *
- * @ZKSim 
- * @version 1.00 2020/7/22
- */
-
-
-public class Transaction_staff {
-	
-	private Staff staffInCharge;
-	private Transactiontype transactiontyp;
-	private double contributionAmt ;
-	
-//consturetor for staff transaction
-
-    public Transaction_staff(Staff staffInCharge,Transactiontype transactiontyp,double conAmt) {
-    	transactionDate(date);
-    	transactionID(ID);
-    	this.staffInCharge =staffInCharge;
-    	this.transactiontyp = transactiontyp;
-    	conAmt=contributionAmt;
-    }
-    
-//geters
-    public Staff getStaffInCharge(){
-    	return staffInCharge;
-    }
-    
-    
-    public Transactiontype getTransactiontyp(){
-    	return transactiontyp;
-    }
-    
-    public getContributionAmt(){
-    	return contributionAmt;
-    }
-    
-//setters
-	public void setStaffInCharge(Staff staffInCharge)
-	{
-	 	this.staffInCharge = staffInCharge;
-	}
-	
-	public void setTransactiontyp(Transactiontype transactiontyp)
-	{
-	 	this.transactiontyp = transactiontyp;
-	}
-	
-	public void setContributionAmt(double contributionAmt)
-	{
-	 	this.contributionAmt = contributionAmt;
-	}
--- Transaction_staff.java
+++ Transaction_staff.java
@@ -6,11 +6,48 @@
  * @version 1.00 2020/7/22
  */
 
+public class Transaction_staff {
+	
+	private Staff staffInCharge;
+	private Transactiontype transactiontyp;
+	private double contributionAmt ;
+	
+//consturetor for staff transaction
 
-public class Transaction_Customers {
-
-    public Transaction_Customers() {
+    public Transaction_staff(Staff staffInCharge,Transactiontype transactiontyp,double conAmt) {
+    	transactionDate(date);
+    	transactionID(ID);
+    	this.staffInCharge =staffInCharge;
+    	this.transactiontyp = transactiontyp;
+    	conAmt=contributionAmt;
+    }
+    
+//geters
+    public Staff getStaffInCharge(){
+    	return staffInCharge;
     }
     
     
-}
+    public Transactiontype getTransactiontyp(){
+    	return transactiontyp;
+    }
+    
+    public getContributionAmt(){
+    	return contributionAmt;
+    }
+    
+//setters
+	public void setStaffInCharge(Staff staffInCharge)
+	{
+	 	this.staffInCharge = staffInCharge;
+	}
+	
+	public void setTransactiontyp(Transactiontype transactiontyp)
+	{
+	 	this.transactiontyp = transactiontyp;
+	}
+	
+	public void setContributionAmt(double contributionAmt)
+	{
+	 	this.contributionAmt = contributionAmt;
+	
