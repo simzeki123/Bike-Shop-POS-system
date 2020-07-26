4041e859c5d0e6b7185387ca4e890ab38fd2cc32
enderlord1583 <40665160+enderlord1583@users.noreply.github.com>
2020-07-26 21:50:27 +0800

    Added class for customer transaction and staff transaction

---
+++ Transaction_staff.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Transaction_Customers.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/7/22
+ */
+
+
+public class Transaction_Customers {
+
+    public Transaction_Customers() {
+    }
+    
+    
+}
