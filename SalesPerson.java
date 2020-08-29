2d2a6de0229dd7eaee3e13ff7091ab5486557647
tenghung19 <tenghung19@gmail.com>
2020-08-29 15:06:53 +0800

    Testing on pushing codes

--- SalesPerson.java
+++ SalesPerson.java
@@ -1,6 +1,40 @@
 public class SalesPerson {
+    private String id;
+    private String name;
+    private String gender;
+    private double basicSalary;
+    private double allowance;
     
     public SalesPerson(){
         
     }
+
+    public SalesPerson(String id, String name, String gender,double basicSalary, double allowance){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.basicSalary = basicSalary;
+        this.allowance = allowance;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public double getBasicSalary(){
+        return basicSalary;
+    }
+
+    public double getAllowance(){
+        return allowance;
+    }
+
 }
