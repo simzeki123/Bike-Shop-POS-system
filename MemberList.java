406ee9f9f2a4ae66ac35e88d360ea4a5c85b4b51
StitchBae <leeyixuan0626@gmail.com>
2020-09-03 09:19:37 +0800

    a

--- MemberList.java
+++ MemberList.java
@@ -5,36 +5,36 @@ public class MemberList{
 	public List<Membership> listOfMember = createListOfMember();
 
     public List<Membership> createListOfMember(){
-        listOfmembers = new ArrayList<>();
+        listOfMember = new ArrayList<>();
 
-        Member[] memberBase = new Member[100];
+        Membership[] memberBase = new Membership[100];
 
-        memberBase[0] = new Member("S0001", "TOM PARKER", "MALE", "010203020394", "Premium");
-        memberBase[1] = new Member("S0002", "CHRIS DOWNEY", "MALE", "020182029382", "Basic");
-        memberBase[2] = new Member("S0003", "LEE SANG HYEOK", "MALE", "010362754932", "Premium");
-        memberBase[3] = new Member("S0004", "SABRINA CHNG", "FEMALE", "982719278372", "Premium");
-        memberBase[4] = new Member("S0005", "EMMA WATSON", "FEMALE", "985467398432", "Premium");
-        memberBase[5] = new Member("S0006", "BILL NEWTON", "MALE", "6217392837284", "Premium");
-        memberBase[6] = new Member("S0007", "SERAPHINE", "FEMALE", "7456349438934", "Basic");
-        memberBase[7] = new Member("S0008", "ALBERT HAWKING", "MALE", "65473628374", "Basic");
-        memberBase[8] = new Member("S0009", "CYNTHIA ENG", "FEMALE", "876543675436", "Premium");
-        memberBase[9] = new Member("S0010", "MOK YEN MEI", "FEMALE", "726362716362", "Basic");
+        memberBase[0] = new Membership( "TOM PARKER", "MALE", "010203020394", "Premium", "0127458412");
+        memberBase[1] = new Membership( "CHRIS DOWNEY", "MALE", "020182029382", "Basic", "");
+        memberBase[2] = new Membership( "LEE SANG HYEOK", "MALE", "010362754932", "Premium", "");
+        memberBase[3] = new Membership( "SABRINA CHNG", "FEMALE", "982719278372", "Premium", "");
+        memberBase[4] = new Membership( "EMMA WATSON", "FEMALE", "985467398432", "Premium", "");
+        memberBase[5] = new Membership( "BILL NEWTON", "MALE", "6217392837284", "Premium", "");
+        memberBase[6] = new Membership( "SERAPHINE", "FEMALE", "7456349438934", "Basic", "");
+        memberBase[7] = new Membership( "ALBERT HAWKING", "MALE", "65473628374", "Basic", "");
+        memberBase[8] = new Membership( "CYNTHIA ENG", "FEMALE", "876543675436", "Premium", "");
+        memberBase[9] = new Membership( "MOK YEN MEI", "FEMALE", "726362716362", "Basic", "");
 
-        listOfmember.add(memberBase[0]);
-        listOfmember.add(memberBase[1]);
-        listOfmember.add(memberBase[2]);
-        listOfmember.add(memberBase[3]);
-        listOfmember.add(memberBase[4]);
-        listOfmember.add(memberBase[5]);
-        listOfmember.add(memberBase[6]);
-        listOfmember.add(memberBase[7]);
-        listOfmember.add(memberBase[8]);
-        listOfmember.add(memberBase[9]);
+        listOfMember.add(memberBase[0]);
+        listOfMember.add(memberBase[1]);
+        listOfMember.add(memberBase[2]);
+        listOfMember.add(memberBase[3]);
+        listOfMember.add(memberBase[4]);
+        listOfMember.add(memberBase[5]);
+        listOfMember.add(memberBase[6]);
+        listOfMember.add(memberBase[7]);
+        listOfMember.add(memberBase[8]);
+        listOfMember.add(memberBase[9]);
 
-        return listOfmember;
+        return listOfMember;
     }
 
-    public List<member> getListOfStaff() {
-        return new ArrayList<member>(listOfmembers);
+    public List<Membership> getListOfMember() {
+        return new ArrayList<Membership>(listOfMember);
     }
 }
--- Membership.java
+++ Membership.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Membership {
 	private String memberID;
 	private String name;
@@ -5,6 +7,7 @@ public class Membership {
 	private String memberIC;
 	private String password;
 	private String memberType; //Premium, Basic
+	private String memberNo;
 
 	
 
@@ -12,16 +15,12 @@ public class Membership {
     	
     }
     
-    public Membership(String memberID, String name, String gender, String memberIC, String memberType){
-    	this.memberID = memberID;
+    public Membership(String name, String gender, String memberIC, String memberType, String memberNo){
     	this.name = name;
     	this.gender = gender;
     	this.memberIC = memberIC;
-    	this.memberType = memberType;
-    }
-    
-    public String getMemberID(){
-    	return memberID;
+		this.memberType = memberType;
+		this.memberNo = memberNo;
     }
     
     public String getName(){
@@ -38,48 +37,197 @@ public class Membership {
     
     public String getMemberType(){
     	return memberType;
-    }
+	}
+	
+	public String getMemberNo(){
+		return memberNo;
+	}
     
-    public String setMemberID(String memberID){
-    	this.memberID = memberID;
-    }
-    
-    public String setName(String name){
+    public void setName(String name){
     	this.name = name;
     }
     
-    public String setGender(String gender){
+    public void setGender(String gender){
     	this.gender = gender;
     }
     
-    public String setMemberIC(String memberIC){
+    public void setMemberIC(String memberIC){
     	this.memberIC = memberIC;
     }
     
-    public String setMemberType(String memberType){
+    public void setMemberType(String memberType){
     	this.memberType = memberType;
     }
-    
+	
+	public void setMemberNo(String memberNo){
+		this.memberNo = memberNo;
+	}
+
     public String toString(){
-    	return String.format("%s\t\t%s\t\t%s\t\t%s\t\t%s\n", memberID, name, gender, memberIC, memberType);
+    	return String.format("%s\t\t%s\t\t%s\t\t%s\t\t%s\n", name, gender, memberIC, memberType, memberNo);
     }
     
-    static String member[] = new member[100];
+    static Membership[] memberBase = new Membership[100];
     
     public static void displayMember(){
-    	
+		MemberList memberBase = new MemberList();
+		System.out.print("+=============================+\n");
+        System.out.print("|      MEMBERSHIP DISPLAY      |\n");
+        System.out.print("+=============================+\n");
+        System.out.println("List of the Member: ");
+        System.out.println("No.\tName\t\t\tGender\t\tIdentity No.\tMembership Type\tContact No.\n");
     }
     
-    public static void addMember(){
-    	
+    public static void addMember(){ //Add new member ot the record
+		Scanner input = new Scanner(System.in);
+		MemberList addMember = new MemberList();
+		Membership member = new Membership();
+		char cfm;
+		int count =0;
+		char cont;
+
+		System.out.print("+==========================+\n");
+        System.out.print("|       Add new Member     |\n");
+        System.out.print("+==========================+\n");
+
+		for (int i = 0; i < addMember.getListOfMember().size(); i++) {
+            memberBase[i] = addMember.listOfMember.get(i);
+            count++;
+        }
+
+		do{
+			System.out.println("Please enter the customer name: "); //add the name of the customer
+			member.setName(input.nextLine());
+			for(int i=0; i < member.getName().length(); i++){
+				while(!Character.isLetter(member.getName().charAt(i))){
+					System.out.println("Invalid Name! Please try again!");
+					System.out.println("Please enter the customer name: ");
+					member.setName(input.nextLine());
+				}
+			}
+
+			System.out.println("Please enter the gender of the customer (MALE/FEMALE): "); //insert the gender of the customer
+			member.setGender(input.nextLine());
+			while(!member.getGender().toUpperCase().equals("MALE")&&!member.getGender().toUpperCase().equals("FEMALE")){
+				System.out.println("Invalid Gender! Please try again!");
+				System.out.println("Please enter the customer gender (MALE/FEMALE): ");
+				member.setGender(input.nextLine());
+			}
+
+			System.out.println("Please enter the customer's IC No. (Without '-'): "); //the IC No. of the customer
+			member.setMemberIC(input.nextLine());
+
+			System.out.println("Please enter the membership type of the customer (Premium, Deluxe, Basic): "); //the membership type of the customer
+			member.setMemberType(input.nextLine());
+			while(member.getMemberType().equals("Premium")&&!member.getMemberType().equals("Deluxe")&&!member.getMemberType().equals("Basic")){
+				System.out.println("Invalid Input! Please enter again!");
+				System.out.println("Please enter the membership type (Premium, Deluxe, Basic): ");
+				member.setMemberType(input.nextLine());
+			}
+
+			System.out.println("Please enter the customer contact no. (Without '-'): ");
+			member.setMemberNo(input.nextLine());
+
+			input.nextLine();
+			System.out.println("Confirm to add new Member? (Y/N): ");
+			cfm  = input.nextLine().charAt(0);
+			while(Character.toUpperCase(cfm)!= 'Y' && Character.toUpperCase(cfm)!= 'N'){
+				System.out.println("Invalid Input! Please enter again!");
+				System.out.println("Confirm to add new Member? (Y/N): ");
+				cfm = input.nextLine().charAt(0);
+			}
+
+			if(Character.toUpperCase(cfm) == 'Y'){
+				memberBase[count] = new Membership(member.getName(), member.getGender(), member.getMemberIC(), member.getMemberType(), member.getMemberNo());
+				addMember.listOfMember.add(memberBase[count]);
+			}
+
+			//else
+
+			System.out.println("Continue to add more member? (Y/N): ");
+			cont = input.nextLine().charAt(0);
+			while(Character.toUpperCase(cont)!= 'Y'&& Character.toUpperCase(cont)!='N'){
+				System.out.println("Invalid Input! Please try again!");
+				System.out.println("Continue to add more member? (Y/N): ");
+				cont = input.nextLine().charAt(0);
+			}
+		}while(Character.toUpperCase(cont)=='Y');
+
+		System.out.println("Member List");
+        System.out.println("No.\tMember ID\tName\t\t\tGender\t\tIdentity No.\tMembership Type\t\tContact No.\n");
+        for (int i = 0; i < addMember.getListOfMember().size(); i++) {
+            System.out.println((i + 1) + "\t" + addMember.getListOfMember().get(i));
+        }
+
     }
     
-    public static void modifyMember(){
-    	
+    public static void modifyMember(){ //Modify the present member record
+		Scanner input = new Scanner(System.in);
+		MemberList modifyMember = new MemberList();
+		Membership member = new Membership();
+
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+
+		//System.out.println("Please enter the member ID that you want to modify: "); //Using member ID to select
+		
+
+		System.out.println("Please enter the new member name: "); //get new changed name
+		member.setName(input.nextLine());
+
+		System.out.println("Please enter the new gender (MALE/FEMALE): "); //get new changed gender
+		member.setGender(input.nextLine());
+
+		System.out.println("Please enter the new member IC No.: "); //get new changed ic
+		member.setMemberIC(input.nextLine());
+
+		System.out.println("Please enter the new membership type (Premium, Deluxe, Basic): "); //get new changed membership type
+		member.setMemberType(input.nextLine());
+
+		System.out.println("Please enter the new contact no. (without '-'): ");
+		member.setMemberNo(input.nextLine());
     }
     
     public static void searchMember(){
-    	
+		Scanner input = new Scanner(System.in);
+		MemberList searchMember = new MemberList();
+		Membership member = new Membership();
+		int choice = 0;
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		System.out.println("Please enter the details that you want to search: ");
+		choice = input.nextInt();
+
+		switch(choice){
+			case 1:
+				System.out.println("Please enter the member ID: ");
+				break;
+				//get member ID
+			case 2:
+				System.out.println("Please enter the name: ");
+				member.setName(input.nextLine());
+				break;
+			case 3:
+				System.out.println("Please enter the gender (MALE/FEMALE): ");
+				member.setGender(input.nextLine());
+				break;
+			case 4:
+				System.out.println("Please enter the IC No.: ");
+				member.setMemberIC(input.nextLine());
+				break;
+			case 5:
+				System.out.println("Please enter the membership type (Premium, Deluxe, Basic): ");
+				member.setMemberType(input.nextLine());
+				break;
+			case 6:
+				System.out.println("Please enter the contact no. (without '-'): ");
+				member.setMemberNo(input.nextLine());
+				break;
+		}
     }
     
     
