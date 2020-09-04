86c3f86f3be4d36fc009ba282810ce2b4ddc9225
tenghung19 <tenghung19@gmail.com>
2020-09-04 09:03:27 +0800

    Updated PosMain(dont know it is correct or not)

--- PosMain.java
+++ PosMain.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * @(#)PosMain.java
  *
@@ -24,6 +25,114 @@ public class PosMain {
         int productCount =0;
         Order[] ords = new Order[100];
         int orderCount = 0;
+
+        Scanner input = new Scanner(System.in);
+        int slct;
+        int choice;
+
+        System.out.print("+================================+\n");
+        System.out.print("|     Bike Rental POS System     |\n");
+        System.out.print("+================================+\n");
+        System.out.print("| 1. STAFF                       |\n");
+        System.out.print("| 2. CUSTOMER                    |\n");
+        System.out.print("| 3. Exit                        |\n");
+        System.out.print("+================================+\n");
+        System.out.print("Please select one of the selection: ");
+        slct = input.nextInt();
+        while(slct < 1 || slct > 3){
+            System.out.print("Invalid input!!\n");
+            System.out.print("<!! Please select between 1-2 !!>\n");
+            System.out.print("Please select one of the selection: ");
+            slct = input.nextInt();
+        }
+
+        switch(slct){
+            case 1:
+                System.out.print("+================================+\n");
+                System.out.print("|     Bike Rental POS System     |\n");
+                System.out.print("+================================+\n");
+                System.out.print("| 1. Bike Product                |\n");
+                System.out.print("| 2. SalesPerson                 |\n");
+                System.out.print("| 3. Membership                  |\n");
+                System.out.print("| 4. Make Order                  |\n");
+                System.out.print("| 5. Get Daily Report            |\n");
+                System.out.print("| 6. Get Monthly Report          |\n");
+                System.out.print("| 7. Exit                        |\n");
+                System.out.print("+================================+\n");
+                System.out.print("Please select one of the selection: ");
+                choice = input.nextInt();
+                while(choice < 1 || choice > 6){
+                    System.out.print("Invalid input!!\n");
+                    System.out.print("<!! Please select between 1-6 !!>\n");
+                    System.out.print("Please select one of the selection: ");
+                    choice = input.nextInt();
+                }
+                
+                switch(choice){
+                    case 1:
+                        Product.getProduct();
+                        break;
+
+                    case 2:
+                        Staff.getStaff();
+                        break;
+
+                    case 3:
+                        TestMembership.getMember();
+                        break;
+
+                    case 4:
+                        Order.order();
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                    default:
+                        System.out.print("Invalid input!!\n");    
+                }
+                break;
+
+            case 2:
+                System.out.print("+================================+\n");
+                System.out.print("|     Bike Rental POS System     |\n");
+                System.out.print("+================================+\n");
+                System.out.print("| 1. Make Order                  |\n");
+                System.out.print("| 2. Exit                        |\n");
+                System.out.print("+================================+\n");
+                System.out.print("Please select one of the selection: ");
+                choice = input.nextInt();
+                while(choice < 1 || choice > 2){
+                    System.out.print("Invalid input!!\n");
+                    System.out.print("<!! Please select between 1-2 !!>\n");
+                    System.out.print("Please select one of the selection: ");
+                    choice = input.nextInt();
+                }
+
+                switch(choice){
+                    case 1:
+                        Order.order();
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        System.out.print("Invalid input!!\n");
+                }
+
+                break;
+            
+            case 3:
+                break;
+
+            default:
+                System.out.print("Invalid input!!\n");
+        }
         
     }
 }
