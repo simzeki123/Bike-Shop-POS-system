089d49b56c077286c43c8cb118ad09739e9659a6
tenghung19 <tenghung19@gmail.com>
2020-09-04 21:10:38 +0800

    updated POSMAIN

--- PosMain.java
+++ PosMain.java
@@ -21,6 +21,7 @@ public class PosMain {
      */
     public static void main(String[] args) {
         Product product = new Product();
+        Staff staff = new Staff();
 
         Membership[] member = new Membership[100];
         int memCount = 0;
@@ -30,119 +31,58 @@ public class PosMain {
         int orderCount = 0;
 
         Scanner input = new Scanner(System.in);
-        int slct = -1;
-        int choice;
+        int choice = 0;
 
         System.out.print("+================================+\n");
         System.out.print("|     Bike Rental POS System     |\n");
         System.out.print("+================================+\n");
-        System.out.print("| 1. STAFF                       |\n");
-        System.out.print("| 2. CUSTOMER                    |\n");
-        System.out.print("| 3. Exit                        |\n");
+        System.out.print("| 1. Bike Product                |\n");
+        System.out.print("| 2. SalesPerson                 |\n");
+        System.out.print("| 3. Membership                  |\n");
+        System.out.print("| 4. Make Order                  |\n");
+        System.out.print("| 5. Get Daily Report            |\n");
+        System.out.print("| 6. Get Monthly Report          |\n");
+        System.out.print("| 7. Exit                        |\n");
         System.out.print("+================================+\n");
         System.out.print("Please select one of the selection: ");
-        
-            
+        do{
             try {
-				slct = input.nextInt();
-			  } catch (InputMismatchException  e) {
-				input.next();
-				System.out.println("Something went wrong.\n");}
-
-        while(slct < 1 || slct > 3){
-            System.out.print("Invalid input!!\n");
-            System.out.print("<!! Please select between 1-2 !!>\n");
-            System.out.print("Please select one of the selection: ");
-            slct = input.nextInt();
-        }
-
-        switch(slct){
-            case 1:
-                System.out.print("+================================+\n");
-                System.out.print("|     Bike Rental POS System     |\n");
-                System.out.print("+================================+\n");
-                System.out.print("| 1. Bike Product                |\n");
-                System.out.print("| 2. SalesPerson                 |\n");
-                System.out.print("| 3. Membership                  |\n");
-                System.out.print("| 4. Make Order                  |\n");
-                System.out.print("| 5. Get Daily Report            |\n");
-                System.out.print("| 6. Get Monthly Report          |\n");
-                System.out.print("| 7. Exit                        |\n");
-                System.out.print("+================================+\n");
-                System.out.print("Please select one of the selection: ");
                 choice = input.nextInt();
-                while(choice < 1 || choice > 6){
-                    System.out.print("Invalid input!!\n");
-                    System.out.print("<!! Please select between 1-6 !!>\n");
-                    System.out.print("Please select one of the selection: ");
-                    choice = input.nextInt();
-                }
-                
-                switch(choice){
-                    case 1:
-                    product.getProduct();
-                        break;
-
-                    case 2:
-
-                        break;
-
-                    case 3:
-
-                        break;
-
-                    case 4:
-
-                        break;
-
-                    case 5:
-                        break;
-
-                    case 6:
-                        break;
-
-                    case 7:
-                        break;
-
-                    default:
-                        System.out.print("Invalid input!!\n");    
-                }
+            } catch (InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Something went wrong\n");
+                System.out.println("Please enter an integer\n");
+            }
+        }while(choice < 1 || choice > 7);
+        
+        switch(choice){
+            case 1:
+                product.getProduct();
                 break;
 
             case 2:
-                System.out.print("+================================+\n");
-                System.out.print("|     Bike Rental POS System     |\n");
-                System.out.print("+================================+\n");
-                System.out.print("| 1. Make Order                  |\n");
-                System.out.print("| 2. Exit                        |\n");
-                System.out.print("+================================+\n");
-                System.out.print("Please select one of the selection: ");
-                choice = input.nextInt();
-                while(choice < 1 || choice > 2){
-                    System.out.print("Invalid input!!\n");
-                    System.out.print("<!! Please select between 1-2 !!>\n");
-                    System.out.print("Please select one of the selection: ");
-                    choice = input.nextInt();
-                }
+                staff.getStaff();
+                break;
 
-                switch(choice){
-                    case 1:
-                        Order.order();
-                        break;
-                    case 2:
-                        break;
-                    default:
-                        System.out.print("Invalid input!!\n");
-                }
+            case 3:
+                
+                break;
+
+            case 4:
 
                 break;
-            
-            case 3:
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
                 break;
 
             default:
-                System.out.print("Invalid input!!\n");
+                System.out.print("Invalid input!!\n");    
         }
-        
     }
 }
